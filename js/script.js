8bb77b2043fe8ebbda02fa797ccba189a396dec4
Lykov Ilya <ilya.lykov.program@gmail.com>
2022-11-09 16:04:41 +0400

    add some functinal, make refactor

--- js/script.js
+++ js/script.js
@@ -14,59 +14,84 @@
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+window.addEventListener("DOMContentLoaded", () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
-const mainElement = document.querySelector(".promo"),
-    promoMenu = mainElement.querySelector(".promo__menu"),
-    promoContent = mainElement.querySelector(".promo__content"),
-    promoInteractive = mainElement.querySelector(".promo__interactive"),
-    promoAdv = mainElement.querySelectorAll(".promo__adv img");
+    removeItems(document.querySelectorAll(".promo__adv"));
+    changeInnerText(document.querySelector(".promo__genre"), "Драма".toUpperCase());
+    changeBg(document.querySelector(".promo__bg"), "/img/bg.jpg");
+    showSortedList(document.querySelector(".promo__interactive-list"), movieDB.movies, returnMovieItem);
 
-promoAdv.forEach(value => {
-    value.remove();
+
+    function returnMovieItem(movie, index) {
+        return (`<li class="promo__interactive-item">${index + 1}. ${movie}<div class="delete"></div></li>`);
+    }
+
+    function showSortedList(elements, array, func) {
+        elements.innerHTML = "";
+        array.sort();
+        array.forEach((element, index) => {
+            elements.innerHTML += func(element, index);
+        });
+    }
+
+    function changeBg(element, path) {
+        element.style.background = `url(${path})center center/cover no-repeat`;
+    }
+
+    function changeInnerText(element, text) {
+        element.innerText = text;
+    }
+
+    function removeItems(elements) {
+        elements.forEach(element => {
+            element.innerHTML = "";
+        });
+    }
+
+    // console.log(promoInteractiveList);
+
+    /* Задания на урок:
+    
+    1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+    новый фильм добавляется в список. Страница не должна перезагружаться.
+    Новый фильм должен добавляться в movieDB.movies.
+    Для получения доступа к значению input - обращаемся к нему как input.value;
+    P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+    
+    2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+    
+    3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+    
+    4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+    "Добавляем любимый фильм"
+    
+    5) Фильмы должны быть отсортированы по алфавиту */
+
+    eventStarter(document.querySelector(".add"), null, [
+        document.querySelector(".adding__input"),
+        document.querySelector('[type = "checkbox"]'),
+        document.querySelector("button")
+    ]);
+
+    function eventStarter(element, func, arrElements) {
+        element.addEventListener("submit", (event) => {
+            event.preventDefault();
+            for (let element in arrElements) {
+                console.log(element);
+            }
+            console.log("click");
+        });
+
+    }
+
 });
 
-const promoBg = promoContent.querySelector(".promo__bg"),
-    promoGenre = promoBg.querySelector(".promo__genre"),
-    promoTitle = promoBg.querySelectorAll(".promo__title"),
-    promoDescr = promoBg.querySelectorAll(".promo__descr"),
-    promoRetings = promoBg.querySelectorAll(".promo__ratings");
-
-promoGenre.textContent = "Драма".toUpperCase();
-
-promoBg.style.background = "url(../img/bg.jpg)";
-
-const buffer = [];
-const promoInteractiveTitleContainer = promoInteractive.querySelector("div"),
-    promoInteractiveList = promoInteractiveTitleContainer.querySelector(".promo__interactive-list"),
-    promoInteractiveTitle = promoInteractiveTitleContainer.querySelector(".promo__interactive-title");
-
-const promoInteractiveItem = promoInteractiveList.querySelectorAll(".promo__interactive-item");
-
-promoInteractiveItem.forEach(value => {
-    buffer.push(value.innerText);
-});
-buffer.sort();
-
-for (let i = 0; i < buffer.length; i++) {
-    promoInteractiveItem[i].innerText = i + 1 + ". " + buffer[i];
-}
-console.dir(promoInteractiveList);
-promoInteractiveList.remove();
-promoInteractiveTitle.remove();
-
-const newPromoInteractiveList = document.createElement("ol");
-promoInteractiveTitleContainer.prepend(newPromoInteractiveList);
-newPromoInteractiveList.className = "promo__interactive-list";
-promoInteractiveItem.forEach(value => {
-    newPromoInteractiveList.append(value);
-});
-promoInteractiveTitleContainer.prepend(promoInteractiveTitle);
